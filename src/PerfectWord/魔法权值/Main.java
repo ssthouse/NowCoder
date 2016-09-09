ffa39e3a814ba67c442e141680a324969994a5f0
ssthouse <ssthouse@ssthousedeMacBook-Pro.local>
2016-09-09 09:44:50 +0800

    added: String 全排列 实现

--- src/PerfectWord/魔法权值/Main.java
+++ src/PerfectWord/魔法权值/Main.java
@@ -1,5 +1,7 @@
 package PerfectWord.魔法权值;
 
+import java.util.*;
+
 /**
  * TODO:
  * not done yet
@@ -8,7 +10,37 @@ package PerfectWord.魔法权值;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int k = scanner.nextInt();
 
+        List<String> strList = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            strList.add(scanner.next());
+        }
+        List<String> allPermutationStr = new ArrayList<>();
+        fullPermutation(allPermutationStr, strList, new StringBuilder());
+        for (String str : allPermutationStr) {
+            System.out.println(str);
+        }
+    }
+
+    public static void fullPermutation(List<String> resultList, List<String> strList, StringBuilder curSb) {
+        //弹出条件
+        if (strList.size() == 0) {
+            resultList.add(curSb.toString());
+            return;
+        }
+        //将list中所有str  依次加入  进行递归
+        for (int i = 0; i < strList.size(); i++) {
+            String str = strList.get(i);
+            curSb.append(str);
+            strList.remove(i);
+            fullPermutation(resultList, strList, curSb);
+            //还原数据  节省内存
+            strList.add(i, str);
+            curSb.delete(curSb.length() - str.length(), curSb.length());
+        }
     }
 
     public static int getWeight(String string) {
@@ -17,6 +49,9 @@ public class Main {
         for (int i = 0; i < string.length(); i++) {
 //            tempStr = tempStCr.substring(1, tempStr.length());
         }
+
+        List list = new ArrayList<>();
+        Set set = new HashSet<>();
         return 1;
     }
 
