e18a6878515f3da67d38ada80a651dbc24cf0a6b
ssthouse <ssthouse@163.com>
2016-09-13 13:59:32 +0800

    add: 网易  another failure

---
+++ src/LookingForOffer/_11_打印1到n位整数/Main.java
@@ -0,0 +1,7 @@
+package LookingForOffer._11_打印1到n位整数;
+
+/**
+ * Created by ssthouse on 16/9/11.
+ */
+public class Main {
+}
--- src/MeiTuan/Num1/Main.java
+++ src/MeiTuan/Num1/Main.java
@@ -1,5 +1,7 @@
 package MeiTuan.Num1;
 
+import java.util.Arrays;
+
 /**
  * Created by ssthouse on 16/9/9.
  */
@@ -8,7 +10,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hahaha");
+        String a = "";
+        int b = 012;
 
-
+        String[] array = {"c", "b", "a"};
+        Arrays.sort(array);
+        String[] newArray = Arrays.copyOf(array, array.length);
+        for (String str : newArray) {
+            System.out.println(str);
+        }
     }
+
 }
