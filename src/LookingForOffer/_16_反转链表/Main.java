73da6b39fb0ba6cbea310761e0aaad2838dfde45
ssthouse <ssthouse@163.com>
2016-09-27 14:18:17 +0800

    complete: 反转链表的递归实现, 只传递一个参数

--- src/LookingForOffer/_16_反转链表/Main.java
+++ src/LookingForOffer/_16_反转链表/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
     /**
      * 使用栈实现
+     *
      * @param node
      * @return
      */
@@ -35,6 +36,12 @@ public class Main {
         return headNode;
     }
 
+    /**
+     * 迭代实现
+     *
+     * @param node
+     * @return
+     */
     public Node reverseIterate(Node node) {
         Node preNode = null;
         Node nextNode = null;
@@ -52,34 +59,40 @@ public class Main {
     /**
      * TODO
      * 递归  传递一个参数的实现
+     *
      * @param node
      * @return
      */
     public Node reverseRecursive(Node node) {
+        if (node == null) {
+            return null;
+        }
         if (node.nextNode == null) {
             return node;
         }
-        Node nextNode = node.nextNode;
         Node resultNode = reverseRecursive(node.nextNode);
-
-
+        node.nextNode.nextNode = node;
+        node.nextNode = null;
         return resultNode;
     }
 
-    
+
     public static void main(String[] args) {
         Main main = new Main();
         //result One
+        Utils.log("用栈实现反转");
         Node headNode = Utils.generateTenNode();
         Node newHead = main.reverse(headNode);
         Utils.printNodeList(newHead);
         Utils.printLine("*");
         //result Two
+        Utils.log("迭代实现");
         headNode = Utils.generateTenNode();
         Node reverseIterateHead = main.reverseIterate(headNode);
         Utils.printNodeList(reverseIterateHead);
         Utils.printLine("*");
         //result Three
+        Utils.log("递归实现: 只传递一个参数");
         headNode = Utils.generateTenNode();
         Node reverseRecursiveHead = main.reverseRecursive(headNode);
         Utils.printNodeList(reverseRecursiveHead);
